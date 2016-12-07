523adfe32ef19a52eea0f6d48b60263078b3120c
David Mandel <mandeldl@gmail.com>
2016-12-07 18:02:44 -0500

    mock some data

--- src/api/index.js
+++ src/api/index.js
@@ -2,11 +2,12 @@
 
 //API Module
 var express = require('express');
+var todos = require('../../mock/todos.json'); //mock data
 
 var router = express.Router();
 
 router.get('/todos', function(req, res) {
-	res.json({'todos':['These are the todos', 'part 2']});
+	res.json({'todos':todos});
 });
 
 module.exports = router;
