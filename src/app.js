236911cadde74be7b465745f23ca4c6f3d4f1a0b
David Mandel <mandeldl@gmail.com>
2016-12-07 17:27:05 -0500

    add router

--- src/app.js
+++ src/app.js
@@ -6,6 +6,14 @@ var app = express();
 
 app.use(express.static('public'));
 
+var router = express.Router();
+
+router.get('/todos', function(req, res) {
+	res.json({'todos':'These are the todos'});
+});
+
+app.use('/api', router);
+
 app.listen('3000', function(){
 	console.log('app is listening on port 3000');
 })
